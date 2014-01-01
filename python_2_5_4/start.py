3fe66f7664ed7822c99efcc266d3919dc2d46123
ShawnDriscoll <shawndriscoll@hotmail.com>
2014-01-01 15:29:11 -0800

    New start file

--- python_2_5_4/start.py
+++ python_2_5_4/start.py
@@ -1,4 +1,14 @@
+#
+#   Run this file to call roll()
+#
+
+"""Performs 10 dice rolls"""
+
 import dice_stuff.diceroll
 
+print 'Welcome to', dice_stuff.version.ver
+dice = '3D6'
+print 'Here are your ten', dice, 'rolls:'
+
 for i in range(10):
-    print dice_stuff.diceroll.roll('2D6')
+    print dice_stuff.diceroll.roll(dice)
