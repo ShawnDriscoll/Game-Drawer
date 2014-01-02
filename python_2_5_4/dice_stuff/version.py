1f995e9c75f206e066c48402c399986985e3d5c5
ShawnDriscoll <shawndriscoll@hotmail.com>
2014-01-01 17:19:39 -0800

    updated __doc__

--- python_2_5_4/dice_stuff/version.py
+++ python_2_5_4/dice_stuff/version.py
@@ -1,6 +1,6 @@
 
-"""This program was written using PyCharm 3.0.2 in a Windows XP/7 environment.
-This code is no longer Beta."""
+"""\nThis program was written using PyCharm 3.0.2 in a Windows XP/7 environment.
+This code is no longer Beta.\n"""
 
 ver = 'roll() release version 1.0.0 for Python 2.5.4'
 vernum = '1.0.0'
