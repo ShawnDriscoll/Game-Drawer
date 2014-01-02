bcfca1e4cd74b075ab594300120b7c29a35efcab
ShawnDriscoll <shawndriscoll@hotmail.com>
2014-01-01 17:42:57 -0800

    updated __doc__

--- python_2_5_4/dice_stuff/__init__.py
+++ python_2_5_4/dice_stuff/__init__.py
@@ -1,5 +1,5 @@
 
-__doc__ = """\nThis program was written using PyCharm 3.0.2 in a
+"""\nThis program was written using PyCharm 3.0.2 in a
 Windows XP/7 environment.  This code is no longer Beta.\n"""
 
 author = 'Shawn Driscoll'
