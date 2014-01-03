7cce8ba7b2ba359924505521ae32c0266112d310
ShawnDriscoll <shawndriscoll@hotmail.com>
2014-01-02 20:10:27 -0800

    updated version number

--- jython_2_5_4rc1/dice_stuff/version.py
+++ jython_2_5_4rc1/dice_stuff/version.py
@@ -2,5 +2,5 @@
 """\nThis program was written using PyCharm 3.0.2 in a Windows XP/7 environment.
 This code is no longer Beta.\n"""
 
-ver = 'roll(), release version 1.0.0 for Jython 2.5.4rc1'
-vernum = '1.0.0'
+ver = 'roll(), release version 1.0.1 for Jython 2.5.4rc1'
+vernum = '1.0.1'
--- python_2_5_4/dice_stuff/version.py
+++ python_2_5_4/dice_stuff/version.py
@@ -2,5 +2,5 @@
 """\nThis program was written using PyCharm 3.0.2 in a Windows XP/7 environment.
 This code is no longer Beta.\n"""
 
-ver = 'roll(), release version 1.0.1b for Python 2.5.4'
-vernum = '1.0.1b'
+ver = 'roll(), release version 1.0.2 for Python 2.5.4'
+vernum = '1.0.2'
