59dc9be65c54628225c0fd007b070c9e5649880c
Casey Webster <casey.webster@gmail.com>
2014-01-02 21:06:51 -0500

    Overhauled diceroll.py from hardcoded values to using regular expressions to handle any valid dice string.

--- python_2_5_4/dice_stuff/diceroll.py
+++ python_2_5_4/dice_stuff/diceroll.py
@@ -8,10 +8,16 @@
 """\nUsage: from diceroll import roll\n"""
 
 from random import randint
+import re
 
 def roll(dice):
     """\nroll() is a dice rolling program.\n\n\
 The types of dice to roll are (in string values):\n\
+roll('<N>D<S>+<M>')
+where N = number of dice (optional)
+      S = number of sides on the dice
+      M = a DM modifier (optional)
+
 roll('1D6') -- roll one 6-sided die\n\
 roll('2D6') -- roll two 6-sided dice\n\
 roll('D10') -- roll a 10-sided die\n\
@@ -21,54 +27,56 @@ roll('D66') -- roll for a D66 chart\n\n\
 roll('3D6+6') -- add +6 DM to roll\n\
 roll('4D4-4') -- add -4 DM to roll\n\n"""
 
-    dice = str(dice)                                                # Make sure dice is a string and not an integer
-    # is there any modifier added?
-    if len(dice) == 3 or (len(dice) == 5) and (dice[3] == '+' or dice[3] == '-'):
-        if dice[0] == 'D':                                          # "D" is at beginning of roll string?
-            if dice[1:3] == '66' and len(dice) == 3:                # D66 rolled?
-                return randint(1, 6) * 10 + randint(1, 6)           # 11 - 66, no DMs allowed
-            if dice[1:3] == '00':                                   # D00 rolled?
-                value = (randint(1, 10) - 1) * 10 + randint(1, 10)  # 1 - 100
-                if len(dice) > 3 and (dice[3] == '+' or dice[3] == '-'):        # Is there a +/- DM to apply?
-                    if dice[3] == '+':                              # A +DM?
-                        value += int(dice[4])
-                    else:                                           # A -DM?
-                        value += -(int(dice[4]))
-                return value
-            if dice[1:3] == '10':                                   # D10 rolled?
-                value = randint(1, 10)                              # 1 - 10
-                if len(dice) > 3 and (dice[3] == '+' or dice[3] == '-'):        # Is there a +/- DM to apply?
-                    if dice[3] == '+':                              # A +DM?
-                        value += int(dice[4])
-                    else:                                           # A -DM?
-                        value += -(int(dice[4]))
-                return value
-        else:
-            if dice[1] == 'D':                                      # "D" is in middle of roll string?
-                if int(dice[0]) >= 1 and int(dice[0]) <= 9:         # How many dice?
-                    value = 0
-                    for i in range(int(dice[0])):                   # Roll the number of dice
-                        value += randint(1, int(dice[2]))           # Add this die roll type to the total
-                    if len(dice) > 3 and (dice[3] == '+' or dice[3] == '-'):        # Is there a +/- DM to apply?
-                        if dice[3] == '+':                          # A +DM?
-                            value += int(dice[4])
-                        else:                                       # A -DM?
-                            value += -(int(dice[4]))
-                    return value
-    print
-    print "** DICE ERROR! '%s' is unknown **" % dice
-    print
-    print "roll() is a dice rolling program."
-    print
-    print "The types of dice to roll are (in string values):"
-    print "roll('1D6') -- roll one 6-sided die"
-    print "roll('2D6') -- roll two 6-sided dice"
-    print "roll('D10') -- roll a 10-sided die"
-    print "roll('D00') -- roll a 100-sided die (1 - 100)"
-    print "roll('D66') -- roll for a D66 chart"
-    print
-    print "-/+ DMs can be added to rolls:"
-    print "roll('3D6+6') -- add +6 DM to roll"
-    print "roll('4D4-4') -- add -4 DM to roll"
-    print
-    return 0
+    # Apply our regular expression to the dice string
+    # To extract the number of dice, sides and modifier
+    dice_info = re.search('([\d+])?D([\d]+)([-\+]\d+)?', str(dice))
+
+    # The supplied dice string was invalid
+    if dice_info == None:
+        print
+        print "** DICE ERROR! '%s' is unknown **" % dice
+        print
+        print "roll() is a dice rolling program."
+        print
+        print "The types of dice to roll are (in string values):"
+        print "roll('<N>D<S>+<M>')"
+        print "where N = number of dice (optional)"
+        print "      S = number of sides on the dice"
+        print "      M = a DM modifier (optional)"
+        print
+        print "Examples:"
+        print
+        print "roll('D20')"
+        print "roll('2D12')"
+        print "roll('3D6+6')"
+        print "roll('4D4-4')"
+        print
+        return 0
+
+    # Sets number of dice to 1 if it was not specified
+    ndice = 1
+    if dice_info.group(1) != None:
+        # Sets number of dice to speficied number
+        ndice = int(dice_info.group(1))
+
+    # Sets dice type (number of sides)
+    dice_type = int(dice_info.group(2))
+
+    # Sets DM to 0 if not specified
+    dice_modifier = 0
+    if dice_info.group(3) != None:
+        # Sets DM to specified value
+        dice_modifier = int(dice_info.group(3))
+
+    # Calculates ndice * randint(1,dicetype) + dice_modifier
+    value = 0
+    # For each dice, roll it
+    for d in range(ndice):
+        # add value of this roll to the total
+        value += randint(1, dice_type) 
+    # Apply the modifier
+    value += dice_modifier
+
+    # Return the dice value to user.
+    return value
+
