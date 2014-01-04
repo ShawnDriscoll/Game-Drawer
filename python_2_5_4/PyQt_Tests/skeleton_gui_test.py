9961dfd6f466a84cf3bd8ad5a6b2c20e9808d9d8
ShawnDriscoll <shawndriscoll@hotmail.com>
2014-01-04 07:38:43 -0800

    PyQtsnipets

---
+++ python_2_5_4/PyQt_Tests/skeleton_gui_test.py
@@ -0,0 +1,49 @@
+"""
+This program creates a skeleton of
+a classic GUI application with a menubar,
+toolbar, statusbar and a central widget.
+"""
+
+import sys
+from PyQt4 import QtGui
+
+class Example(QtGui.QMainWindow):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):               
+        
+        textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAction = QtGui.QAction(QtGui.QIcon('exit24.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+        
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main window')    
+        self.show()
+        
+        
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()    
