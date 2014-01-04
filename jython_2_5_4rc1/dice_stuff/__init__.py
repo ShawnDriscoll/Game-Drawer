787ce47bc0abd7cbbf6255e13ed9dd3d5f352366
ShawnDriscoll <shawndriscoll@hotmail.com>
2014-01-04 01:31:17 -0800

    New __doc__

--- jython_2_5_4rc1/dice_stuff/__init__.py
+++ jython_2_5_4rc1/dice_stuff/__init__.py
@@ -1,6 +1,7 @@
 
-"""\nThis program was written using PyDev 3.2 in a
-Windows XP/7 environment.  This code is no longer Beta.\n"""
+"""\nThis program was written using Eclipse with the
+PyDev 3.2 plug-in in a Windows XP/7 environment.
+This code is no longer Beta.\n"""
 
 author = 'Shawn Driscoll'
 
--- jython_2_5_4rc1/dice_stuff/version.py
+++ jython_2_5_4rc1/dice_stuff/version.py
@@ -1,5 +1,6 @@
 
-"""\nThis program was written using PyDev 3.2 in a Windows XP/7 environment.
+"""\nThis program was written using Eclipse with the
+PyDev 3.2 plug-in in a Windows XP/7 environment.
 This code is no longer Beta.\n"""
 
 ver = 'roll(), release version 1.0.1 for Jython 2.5.4rc1'
