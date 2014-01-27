4b3d60a2f3afa073d77c0f3cd3ef7af3bfd3e418
ShawnDriscoll <shawndriscoll@hotmail.com>
2014-01-27 02:29:57 -0800

    comments

--- jython_2_5_4rc1/dice_stuff/version.py
+++ jython_2_5_4rc1/dice_stuff/version.py
@@ -1,7 +1,9 @@
 
-"""\nThis program was written using Eclipse with the
+"""
+This program was written using Eclipse with the
 PyDev 3.2 plug-in in a Windows XP/7 environment.
-This code is no longer Beta.\n"""
+This code is no longer Beta.
+"""
 
 ver = 'roll(), release version 1.0.2 for Jython 2.5.4rc1'
 vernum = '1.0.2'
