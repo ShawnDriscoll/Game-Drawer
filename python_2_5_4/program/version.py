8354477c57efbebb5f082074fdb9e9efd5de4dc5
ShawnDriscoll <shawndriscoll@hotmail.com>
2017-04-03 02:02:44 -0700

    Dynamic versioning

--- python_2_5_4/program/version.py
+++ python_2_5_4/program/version.py
@@ -5,5 +5,5 @@ PyDev 3.2 plug-in in a Windows XP/7/10 environment.
 This code is no longer Beta.
 """
 
-ver = 'roll(), release version 2.1.0 for Python 2.5.4'
 vernum = '2.1.0'
+ver = 'roll(), release version ' + vernum + ' for Python 2.5.4'
